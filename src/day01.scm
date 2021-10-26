9cc9bcfd15e0cb268e4e5db08e1fd8abccbb513c
Martin Henstridge <mchenstridge@gmail.com>
2021-10-26 12:11:41 +0100

    Day 1

---
+++ src/day01.scm
@@ -0,0 +1,40 @@
+#!/usr/local/bin/csi -ss
+
+(import (chicken io))
+(import srfi-1)
+(import srfi-13)
+
+
+(define (file->list path)
+  (call-with-input-file path read-lines))
+
+
+(define (parse-input lines)
+  (map string->number (remove string-null? lines)))
+
+
+(define (mass->fuel m)
+  (- (quotient m 3) 2))
+
+
+(define (combined-mass->fuel m)
+  (let loop ((total 0) (curr m))
+    (let ((fuel (mass->fuel curr)))
+      (if (<= fuel 0)
+      total
+      (loop (+ total fuel) fuel)))))
+
+
+(define (part-one ms)
+  (foldl + 0 (map mass->fuel ms)))
+
+
+(define (part-two ms)
+  (foldl + 0 (map combined-mass->fuel ms)))
+
+
+(define INPUT (file->list "data/01.txt"))
+(define (main args)
+  (let ((ms (parse-input INPUT)))
+    (print (part-one ms))   ; 3520097
+    (print (part-two ms)))) ; 5277255
