59d1fbb3c6f78547a5b4726ffcf161882d537e72
Martin Henstridge <mchenstridge@gmail.com>
2022-10-29 10:52:28 +0100

    Refactor main

--- src/main.c
+++ src/main.c
@@ -6,22 +6,32 @@ void solve02(const char *input);
 void solve03(const char *input);
 
 
+typedef void (*Solver)(const char *);
+
+typedef struct {
+    Solver solver;
+    char *path;
+} Day;
+
+static const Day days[] = {
+    { solve01, "data/01.txt" },
+    { solve02, "data/02.txt" },
+    { solve03, "data/03.txt" },
+};
+
+static const size_t num_days = sizeof(days) / sizeof(*days);
+
+
 int
 main(int argc, char **argv)
 {
     char *input;
 
-    input = read_file("data/01.txt");
-    solve01(input);
-    free(input);
-
-    input = read_file("data/02.txt");
-    solve02(input);
-    free(input);
-
-    input = read_file("data/03.txt");
-    solve03(input);
-    free(input);
+    for (size_t day = 0; day < num_days; day++) {
+        input = read_file(days[day].path);
+        days[day].solver(input);
+        free(input);
+    }
 
     return EXIT_SUCCESS;
 }
